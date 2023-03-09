65cf0d2a18ffcc1e57a518d43091980e29783c91
Wenslaus Kachasu <wenslausnkachasu@gmail.com>
2023-03-09 19:37:28 +0200

    Updated the input field on the subscribe form to be responsive

--- src/components/layout/Home/SubscribeSection.tsx
+++ src/components/layout/Home/SubscribeSection.tsx
@@ -19,17 +19,17 @@ export default function SubscribeSection() {
         alt="subscribe background image"
       />
       <div className="relative place-items-center text-center max-w-xl mx-auto px-6 py-10">
-        <h1 className="text-5xl text-[#fff] leading-snug font-extrabold z-40">
+        <h1 className="text-2xl lg:text-5xl text-[#fff] leading-snug font-extrabold z-40">
           Never miss an Update
         </h1>
-        <p className="text-2xl text-[#fff] font-normal leading-tight z-[100]">
+        <p className="text-lg lg:text-2xl text-[#fff] font-normal leading-tight z-[100]">
           Subscribe to out Newsletter
         </p>
-        <form className="flex flex-row items-center gap-4 mt-4" onSubmit={handleFormSubmit}>
-          <div className="flex flex-row items-center gap-4 mx-auto">
+        <form className="flex flex-row items-center mt-4" onSubmit={handleFormSubmit}>
+          <div className="flex flex-row items-center mx-auto">
             <input
               type="email"
-              className="w-[800px] h-[62px] rounded border-transparent focus:ring-0 z-40"
+              className="w-[22rem] h-[3.5rem] lg:w-[50rem] lg:h-[3.875rem] lg:rounded border-transparent focus:ring-0 z-40"
               id="email"
               placeholder="Enter your email"
             />
