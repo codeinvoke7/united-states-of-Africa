3cdea31cc0922679922f32a22a903dcd5e0c3b6e
Blessing Okonkwo <theblessingokonkwo@gmail.com>
2023-03-20 17:33:44 +0100

    remove comments and commented-out code

--- src/pages/VolunteerPage/VolunteerView.tsx
+++ src/pages/VolunteerPage/VolunteerView.tsx
@@ -4,7 +4,6 @@ import Volunteering from 'components/layout/Volunteer/Volunteering'
 import WhySection from 'components/layout/Volunteer/WhySection'
 import VideoSection from 'components/layout/Volunteer/VideoSection'
 import { SubscribeSection } from 'components/layout/Home'
-// import { Welcome, Volunteering, WhySection } from 'components/layout/Volunteer';
 
 const VolunteerView = () => {
   return (
