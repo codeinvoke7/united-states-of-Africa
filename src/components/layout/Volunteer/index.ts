c67731e66330e7161c8207f5e76c85fe3e436838
Blessing Okonkwo <theblessingokonkwo@gmail.com>
2023-03-21 12:47:00 +0100

    implement functional component for pages to load from top when route changes

---
+++ src/components/layout/Volunteer/index.ts
@@ -0,0 +1,4 @@
+export { default as VideoSection } from './VideoSection';
+export { default as Volunteering } from './Volunteering';
+export { default as Welcome } from './Welcome';
+export { default as WhySection } from './WhySection';
--- src/pages/VolunteerPage/VolunteerView.tsx
+++ src/pages/VolunteerPage/VolunteerView.tsx
@@ -1,8 +1,5 @@
 import React from 'react'
-import Welcome from 'components/layout/Volunteer/Welcome'
-import Volunteering from 'components/layout/Volunteer/Volunteering'
-import WhySection from 'components/layout/Volunteer/WhySection'
-import VideoSection from 'components/layout/Volunteer/VideoSection'
+import { Welcome, Volunteering, WhySection, VideoSection} from 'components/layout/Volunteer'
 import { SubscribeSection } from 'components/layout/Home'
 
 const VolunteerView = () => {
