b21ab7010a002985146da8c42ebfdd63c11050b7
Victoria <adedayovicky123@gmail.com>
2023-03-28 14:59:00 +0100

    refactor contact form to accomodate rechaptcha

--- src/components/layout/ContactUs/ContactForm.jsx
+++ src/components/layout/ContactUs/ContactForm.jsx
@@ -1,6 +1,25 @@
 import { paperclip } from 'assets/icons';
+import { useState } from 'react';
 
 export default function ContactForm() {
+  const [values, setValues] = useState({
+    firstname: '',
+    lastname: '',
+    email: '',
+    subject: '',
+    message: '',
+    file: ''
+  });
+
+  console.log('v', values);
+
+  const handleChange = (e) => {
+    setValues({
+      ...values,
+      [e.target.id]: e.target.value
+    });
+  };
+
   return (
     <form className="bg-[#ECF9EF] flex justify-center mt-20 mb-5 pb-14 pt-14 font-serif">
       <div className="md:w-96 w-72">
@@ -8,14 +27,30 @@ export default function ContactForm() {
           <label htmlFor="" className="text-lg md:text-xl">
             First Name
           </label>
-          <input type="text" required className="rounded-lg border-[rgba(37, 55, 63, 0.3)]" />
+          <input
+            type="text"
+            id="firstname"
+            name="firstname"
+            value={values.firstname}
+            onChange={handleChange}
+            required
+            className="rounded-lg border-[rgba(37, 55, 63, 0.3)]"
+          />
         </div>
 
         <div className="flex flex-col mb-5">
           <label htmlFor="" className="text-lg md:text-xl">
             Last Name
           </label>
-          <input type="text" required className="rounded-lg border-[rgba(37, 55, 63, 0.3)]" />
+          <input
+            type="text"
+            id="lastname"
+            name="lastname"
+            value={values.lastname}
+            onChange={handleChange}
+            required
+            className="rounded-lg border-[rgba(37, 55, 63, 0.3)]"
+          />
         </div>
 
         <div className="flex flex-col mb-5">
@@ -24,8 +59,10 @@ export default function ContactForm() {
           </label>
           <input
             type="email"
-            name=""
-            id=""
+            name="email"
+            id="email"
+            value={values.email}
+            onChange={handleChange}
             required
             className="rounded-lg border-[rgba(37, 55, 63, 0.3)]"
           />
@@ -35,7 +72,15 @@ export default function ContactForm() {
           <label htmlFor="" className="text-lg md:text-xl">
             Subject
           </label>
-          <input type="text" required className="rounded-lg border-[rgba(37, 55, 63, 0.3)]" />
+          <input
+            type="text"
+            required
+            name="subject"
+            id="subject"
+            value={values.subject}
+            onChange={handleChange}
+            className="rounded-lg border-[rgba(37, 55, 63, 0.3)]"
+          />
         </div>
 
         <div className="flex flex-col mb-5">
@@ -43,8 +88,10 @@ export default function ContactForm() {
             Message
           </label>
           <textarea
-            name=""
-            id=""
+            name="message"
+            id="message"
+            value={values.message}
+            onChange={handleChange}
             cols="30"
             rows="3"
             placeholder="Max. 5000 words"
@@ -55,8 +102,10 @@ export default function ContactForm() {
 
         <input
           type="file"
-          name=""
-          id=""
+          name="file"
+          id="file"
+          value={values.file}
+          onChange={handleChange}
           className=" text-[#39B755] rounded-lg focus:border focus:border-[#39B755] active:border-[#39B755]"
         />
 
